a5bf2359246b88d2b8ca414cca1130d545ae9439
mlcukurcayir <mlcukurcayir@yahoo.com>
2022-09-24 13:20:37 +0300

    ilk commit

---
+++ veritipleri/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+	   int sayi = 125;
+	  
+
+	   char karakter = 'A';
+
+	   boolean dogruMu=false;
+
+    }
+}
+System.out.println(sayi);
